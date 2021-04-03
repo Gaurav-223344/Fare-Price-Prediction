89adca654b796161ef15b5b10f10e196f71c6aab
Gaurav Vinayak Gaikwad <61324204+Gaurav-223344@users.noreply.github.com>
2021-04-03 17:30:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 
-model = pickle.load(open('flight_rf.pkl', 'rb'))
+model = pickle.load(open('flight_rf_1.pkl', 'rb'))
 
 @app.route('/',methods=['GET'])
 def Home():
